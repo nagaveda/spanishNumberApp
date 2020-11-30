e325bf9c22d88bb05f4f3b4f59988cf7c183c9c1
nagaveda <gajjalanagaveda@karunya.edu.in>
2020-11-30 18:38:48 +0530

    Added all project files

---
+++ App.js
@@ -0,0 +1,114 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView
+} from "react-native";
+
+ import Sound from "react-native-sound";
+
+const App = () => {
+  
+  const soundList = [
+    require("./assets/one.wav"),
+    require("./assets/two.wav"),
+    require("./assets/three.wav"),
+    require("./assets/four.wav"),
+    require("./assets/five.wav"),
+    require("./assets/six.wav"),
+    require("./assets/seven.wav"),
+    require("./assets/eight.wav"),
+    require("./assets/nine.wav"),
+    require("./assets/ten.wav"),
+    
+  ];
+
+  const playSound = (sound) =>{
+    
+
+    const soundVar = new Sound(sound, Sound.MAIN_BUNDLE,(err)=>{
+     if(err){
+       console.log(err);
+     }
+    });
+
+    setTimeout(()=>{
+      soundVar.play();
+    }, 1000);
+    
+    soundVar.release();
+
+  }
+  
+  return(
+    <ScrollView style={styles.container}>
+      <Image style={styles.logo} source={require("./assets/logo.png")} />
+      <View>
+        <Text
+        style={styles.text}>
+          Spanish Numbers
+        </Text>
+      </View>
+      
+      <View style={styles.gridContainer}>
+      
+          {soundList.map((sound) => (
+            <TouchableOpacity
+            key={sound}
+            onPress={()=>{
+              playSound(sound)
+            }
+            }
+            style={styles.box}
+            >
+              <Text style={styles.text}>{sound}</Text>
+            </TouchableOpacity>
+          ))}
+        
+      </View>
+    </ScrollView>
+  )
+};
+
+
+export default App;
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#1b262c"
+  },
+  logo:{
+    alignSelf:"center",
+    marginTop:50
+  },
+  gridContainer:{
+    flex:1,
+    margin: 5,
+    flexDirection:"row",
+    flexWrap:"wrap",
+    alignItems:"flex-start",
+    justifyContent:"space-around"
+  },
+  box:{
+    height: 110,
+    alignItems:"center",
+    justifyContent:"center",
+    width: "46%",
+    marginVertical:6,
+    backgroundColor:"#0f4c75",
+    borderRadius: 5,
+    shadowColor:"#393e46",
+    elevation: 5
+
+  },
+  text:{
+    fontSize: 50,
+    color: "#ff4301",
+    textAlign:"center"
+  }
+});
